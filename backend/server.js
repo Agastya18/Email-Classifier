900e6559968bbb25a0e6cddc0b0b1b946c212826
agastya gaur <agastyagaur17@gmail.com>
2024-07-21 00:50:13 +0530

    make some change for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import emailRoutes from './routes/emailRoutes.js';
 dotenv.config();
 const PORT= process.env.PORT || 5000;
 const app = express();
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true,cookie:{secure:false} }));
 app.use(passport.initialize());
@@ -38,7 +38,13 @@ app.use('/emails', emailRoutes);
 // })
 
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port $ http://localhost:${PORT}`);
