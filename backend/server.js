1266bfb94c5e5205e2a5ebb2b0893aeab93b3378
agastya gaur <agastyagaur17@gmail.com>
2024-07-21 10:09:07 +0530

    add code

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(passport.session());
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://email-classifier-lc2v.onrender.com',
         methods: ['GET', 'POST'],
         credentials: true
     }
